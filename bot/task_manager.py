e37c3364bc5693fd067848aa45bc0057e8d4fe11
DavidDaimond <strokatov91@mail.ru>
2021-11-29 23:21:43 +0300

    Some changes and tests in task_manager

--- bot/task_manager.py
+++ bot/task_manager.py
@@ -3,29 +3,40 @@ import threading
 
 
 CHECK_DELTA = datetime.timedelta(minutes=2)
+DEFAULT_REPEAT_TIME = datetime.timedelta(minutes=60 * 24 * 365)
 
 
 class Task:
     def __init__(
             self,
-            user, time, func,
+            time, func,
             repetitive=False,
             check_delta=CHECK_DELTA,
-            name='NoName'
+            name='NoName',
+            repeat_time=DEFAULT_REPEAT_TIME
     ):
-        self.user = user
+
         self.time = time
         self.func = func
         self.repetitive = repetitive
+        self.repeat_time = repeat_time if repetitive else None
         self.name = name
 
         self.check_delta = check_delta
 
+        self.active = True
+
     def __bool__(self):
-        delta = datetime.datetime.now() - self.time
+        delta = abs(datetime.datetime.now() - self.time)
         if self.repetitive:
-            delta = delta - datetime.timedelta(days=delta.days)
+            delta = delta - self.repeat_time
         return delta < self.check_delta
+
+    def do(self):
+        if self and self.active:
+            if not self.repetitive:
+                self.active = False
+            self.func(self)
 
 
 class TaskManager:
@@ -34,17 +45,17 @@ class TaskManager:
 
     def run(self):
         for task in self.tasks:
-            task.func() if task else None
+            task.do()
 
     def add(self, task):
         self.tasks.append(task)
 
 
 class TaskManagerThread(threading.Thread):
-    def __init__(self, tm):
-        super().__init__(group=None)
+    def __init__(self, tm, name=None):
+        super().__init__(group=None, name=name)
         self.tm = tm
 
     def run(self):
         while True:
-            tm.run()
+            self.tm.run()
